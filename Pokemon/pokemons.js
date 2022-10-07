cdfe5c409fd744582b34db2b910d9777da5c7c4c
Alessandro-Vieira <47628979+Alessandro-Vieira@users.noreply.github.com>
2022-10-07 19:33:41 -0300

    Add files via upload

--- Pokemon/pokemons.js
+++ Pokemon/pokemons.js
@@ -1,3 +1,48 @@
+// API endpoint --------------------------------------------
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+// Get Elements --------------------------------------------
+const searchInput = getElement('.search-input'),
+      searchButton = getElement('.search-button'),
+      container = getElement('.pokemon'),
+      erroMessage = getElement('.error');
+
+var pokeName, // Nome ou numero passado na caixa de busca
+    pokemon, // Responsavel por guardar os dados recebidos da API
+    card; // Responsavel por receber o HTML 
+
+// Build Functions --------------------------------------------
+// Função para reduzir a escrita na captura de elementos HTML
+function getElement(element) {
+  return document.querySelector(element);
+}
+
+// Função responsavel por fazer requisições para a API e inserir as respostas na variavel pokemon
+function requestPokeInfo(url, name) {
+  fetch(url + name)
+    .then(response => response.json())
+    .then(data => {
+      pokemon = data;
+    })
+    .catch(err => console.log(err));
+}
+
+// Função que faz a chamada das principais funções e inicia o app
+function startApp(pokeName) {
+  requestPokeInfo(baseUrl, pokeName);
+
+  setTimeout(function () {
+      container.innerHTML = createCard();
+  }, 2000);
+}
+
+// Add Events --------------------------------------------
+searchButton.addEventListener('click', event => {
+  event.preventDefault();
+  pokeName = searchInput.value.toLowerCase();
+  startApp(pokeName);
+});
+
 // Função responsavel por montar o HTML exibido na pagina
 function createCard () {
   card = `
@@ -14,3 +59,4 @@ function createCard () {
     </div>`;
   return card;
 }
+
