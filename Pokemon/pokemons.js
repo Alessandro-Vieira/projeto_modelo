f93dee23d0fe08d72ac50059130bb78475cead37
Alessandro-Vieira <47628979+Alessandro-Vieira@users.noreply.github.com>
2022-10-07 13:45:40 -0300

    Add files via upload

---
+++ Pokemon/pokemons.js
@@ -0,0 +1,16 @@
+// Função responsavel por montar o HTML exibido na pagina
+function createCard () {
+  card = `
+    <div class="pokemon-picture">
+      <img src="${pokemon.sprites.front_default}" alt="Sprite of ${pokemon.name}">
+    </div>
+    <div class="pokemon-info">
+        <h1 class="name">Name: ${pokemon.name}</h1>
+        <h2 class="number">Nº ${pokemon.id}</h2>
+        <h3 class="type">Type: ${pokemon.types.map(item => item.type.name).toString()}</h3>
+        <h3 class="skill">Skills: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
+        <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
+        <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
+    </div>`;
+  return card;
+}
